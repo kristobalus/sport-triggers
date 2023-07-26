03e844cdca96bb427a8706c39e3fa3074774c227
kristobalus <ildar.tamendarov@gmail.com>
2023-07-27 01:09:36 +0600

    fix(metadata): debug logs (#22)

--- src/actions/studio/metadata/get.ts
+++ src/actions/studio/metadata/get.ts
@@ -4,18 +4,24 @@ import { FleetApp } from '../../../fleet-app'
 import { StudioConditionData } from "../../../models/studio/studio.condition-data"
 
 async function Handler(this: FleetApp, request: ServiceRequest) {
-  const { amqp, metadataService } = this
+  const { amqp, metadataService, log } = this
   const { eventId, sport } = request.params as unknown as { eventId: string, sport: string }
+
+  log.debug({  eventId, sport }, `get metadata request`)
 
   // resolve by amqp request
   const response = await amqp.publishAndWait("sports.events.retrieveProviderId",
     { id: eventId, provider: metadataService.getDatasource() },
     { reuse: true, cache: 600 })
 
+  log.debug({  response }, `sl-sports response with resolved provider id`)
+
   const { id: gameId } = response.data
 
   // create metadata
   const data: StudioConditionData = metadataService.getConditionData(gameId, sport)
+
+  log.debug({  data }, `event ui metadata`)
 
   return { data }
 }
