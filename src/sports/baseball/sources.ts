a3633a5459fadfc17ebf280f4e83feee6e3c27ff
sky3d <sky3ddd@gmail.com>
2023-09-07 19:57:45 +0300

    fix(meta): describe pitch type values

--- src/sports/baseball/sources.ts
+++ src/sports/baseball/sources.ts
@@ -3,5 +3,6 @@ export enum Sources {
   PitchOutcome = 'baseball.pitch.outcome',
   AtBatOutcome = 'baseball.atbat.outcome',
   GameStates = 'baseball.game.states',
-  InningHalf = 'baseball.inning.half'
+  InningHalf = 'baseball.inning.half',
+  PitchType = 'baseball.pitch.type',
 }
